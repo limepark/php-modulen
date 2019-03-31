797fbb5ea7f3ad2054a7457ec610761844283a76
Patrik Malmström <popsork@gmail.com>
2019-03-31 19:54:24 +0200

    Update "Created" part to work as expected.

--- examples/byline.php
+++ examples/byline.php
@@ -4,16 +4,19 @@ $propertyUtil       = new PropertyUtil();
 $dateUtil           = new DateUtil();
 $currentPage        = $portletContextUtil->getCurrentPage();
 
-$publishedBy = $propertyUtil->getNestedString($currentPage, 'publishedBy', 'displayName');
 $isPublished = $propertyUtil->getBoolean($currentPage, 'published');
 
 if ($isPublished) {
   $publishDate = $propertyUtil->getCalendar($currentPage, 'publishDate');
+  $publishedBy = $propertyUtil->getNestedString($currentPage, 'publishedBy', 'displayName');
   $publishDateString = $dateUtil->getCalendarAsString('yyyy-MM-dd', $publishDate);
+ 
   echo "<p class=\"normal\">Sidan publicerades $publishDateString av $publishedBy.</p>";
 } else {
   $creationDate = $propertyUtil->getCalendar($currentPage, 'creationDate');
+  $createdBy = $propertyUtil->getNestedString($currentPage, 'createdBy', 'displayName');
   $creationDateString = $dateUtil->getCalendarAsString('yyyy-MM-dd', $creationDate);
-  echo "<p class=\"normal\">Sidan skapades $$creationDateString av $publishedBy.</p>";
+
+  echo "<p class=\"normal\">Sidan skapades $creationDateString av $createdBy.</p>";
 }
 ?>
