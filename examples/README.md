205848bcb0f2c2738679633b31bc74e50ca6a0bc
Patrik Malmström <popsork@gmail.com>
2019-03-31 19:51:23 +0200

    Add byline example

--- examples/README.md
+++ examples/README.md
@@ -4,3 +4,6 @@ Exempel som listar undersidor till nuvarande sida.
 
 ## [Dagens datum](todays-date.php)
 Exempel som skriver ut dagens datum.
+
+## [Byline](byline.php)
+Exempel som skriver ut när sidan publicerades och av vem den publicerades. Ifall sidan inte är publicerad skrivs skapelse-datumet ut.
---
+++ examples/byline.php
@@ -0,0 +1,19 @@
+<?php
+$portletContextUtil = new PortletContextUtil();
+$propertyUtil       = new PropertyUtil();
+$dateUtil			= new DateUtil();
+$currentPage        = $portletContextUtil->getCurrentPage();
+
+$publishedBy = $propertyUtil->getNestedString($currentPage, 'publishedBy', 'displayName');
+$isPublished = $propertyUtil->getBoolean($currentPage, 'published');
+
+if ($isPublished) {
+  $publishDate = $propertyUtil->getCalendar($currentPage, 'publishDate');
+  $publishDateString = $dateUtil->getCalendarAsString('yyyy-MM-dd', $publishDate);
+  echo "<p class=\"normal\">Sidan publicerades $publishDateString av $publishedBy.</p>";
+} else {
+  $creationDate = $propertyUtil->getCalendar($currentPage, 'creationDate');
+  $$creationDateString = $dateUtil->getCalendarAsString('yyyy-MM-dd', $creationDate);
+  echo "<p class=\"normal\">Sidan skapades $$creationDateString av $publishedBy.</p>";
+}
+?>
